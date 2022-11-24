4abca892de16ebc5eaf7fb162a0df15f213712a1
fkrckdzkdl <91944913+fkrckdzkdl@users.noreply.github.com>
2022-11-24 10:34:50 +0900

    추가하기 / 유효성검사 기능 구현완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import {TodoContext} from './TodoWrapper'
+import styles from './Footer.module.css'
 
 function Footer() {
     const {addTodo} = useContext(TodoContext);
+    const [plusTodo,setPlusTodo] = useState();
+
+    // (공백 제거 처리)
+    const HandleChange = (val)=>{
+      console.log(val); 
+      setPlusTodo(val.trim());
+      
+    }
+
+    // 리스트추가 버튼 
+    const handleAdd = ()=>{
+      if(plusTodo.length===0){
+        alert('올바른 값을 입력하세요 !');
+        document.querySelector("#TB").value='';
+        setPlusTodo('');
+        return;
+      }
+      addTodo(plusTodo);
+      document.querySelector("#TB").value='';
+      setPlusTodo('');
+    }
+  
+
     return (
-      <div>
-        <input type='text'></input>
-        <button onClick={addTodo}>add</button>
+      <div className={styles.Footer}>
+        <input id='TB'
+         className={styles.TextBox}
+         type='textfeild'
+         onChange={(event)=>{HandleChange(event.target.value)}}
+         onKeyUp={(event)=>{if(event.key==="Enter") handleAdd()}}/>
+        <button className={styles.AddBtn} onClick={()=>handleAdd()}>add</button>
       </div>
     );
   }
